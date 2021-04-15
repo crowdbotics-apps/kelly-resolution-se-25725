79d7ff940a049eb4efd69b3dd634e446cbfeada3
Crowdbotics <team@crowdbotics.com>
2021-04-15 22:47:51 +0000

    Update models

--- kelly_resolution_se_25725/wsgi.py
+++ kelly_resolution_se_25725/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kelly_resolution_se_25725.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kelly_resolution_se_25725.settings")
 
 application = get_wsgi_application()
